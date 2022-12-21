b57110c0f700febc047913bc72e10e5f23c540fa
nnra6864 <enenragd@gmail.com>
2022-12-21 05:28:23 +0100

    Updated READ ME.md

--- MailSender/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ MailSender/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -236,7 +236,7 @@ namespace MailSender {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Mail Sender;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Mail Sender;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
